b25c16a64603d61574b0c03251f4d1611d21849a
Andrew Yatskovets <jtsandrew@gmail.com>
2011-05-18 17:16:55 +0300

    Added lang file

---
+++ lang/English/strings.php
@@ -0,0 +1,22 @@
+<?php
+$_LANG['ERORR_WRONG_LOGIN_DATA'] = 'Login, password or host incorrect';
+$_LANG['HELLO_'] = 'Hello';
+$_LANG['VITRUAL_MACHINE_DETAILS'] = 'Virtual Machine Details';
+$_LANG['CREATE_VIRTUAL_MACHINE'] = 'Create Virtual Machine';
+$_LANG['LOGIN_'] = 'Log In';
+$_LANG['PASSWORD_'] = 'Password';
+$_LANG['HOST_'] = 'Host';
+$_LANG['SELECT_LANG'] = 'Select language';
+$_LANG['LOGOUT_'] = 'Log Out';
+$_LANG['DASHBOARD_'] = 'Dashboard';
+
+$_LANG['USER_DETAILS'] = 'User Details';
+$_LANG['FULL_NAME'] = 'Fullname';
+$_LANG['E_MAIL'] = 'E-mail';
+$_LANG['TIME_ZONE'] = 'Time zone';
+$_LANG['LOCALE_'] = 'Locale';
+$_LANG['USER_ROLES'] = 'User Roles';
+$_LANG['YOUR_PROFILE_INFORMATION'] = 'Your profile information';
+$_LANG['YOUR_PROFILE'] = 'Your profile';
+$_LANG['PROFILE_'] = 'Profile';
+?>
