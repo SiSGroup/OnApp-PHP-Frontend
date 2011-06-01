7567c85c4343ad78b67ef60a324846062781864b
Andrew Yatskovets <jtsandrew@gmail.com>
2011-06-01 16:40:08 +0300

    Loger functions was added

--- functions.php
+++ functions.php
@@ -7,12 +7,17 @@
  *
  */
 function onapp_get_languageList() {
+
+    onapp_debug("Get languages list");
+
     $languages = scandir('lang');
 
     foreach( $languages as $language )
         if (! is_dir($language) && ! is_file("lang/$language") )
             $result[] = $language;
 
+    onapp_debug("onapp_get_languageList:\nreturn => " . var_export($result, true) );
+    
     return $result;
 }
 
@@ -27,9 +32,19 @@ function onapp_get_languageList() {
 function onapp_string($string) {
     global $_LANG;
 
-    return isset($_LANG[$string]) ?
+//    onapp_debug("Get string" );
+
+    $return = isset($_LANG[$string]) ?
         $_LANG[$string] :
         "String $string not found";
+
+    $return = isset($_LANG[$string]) ?
+        $_LANG[$string] :
+        "String $string not found";
+
+//    onapp_debug("onapp_string: 'string' => '$string', return => $return");
+
+    return $return;
 }
 
 /**
@@ -42,48 +57,55 @@ function onapp_string($string) {
  */
 function onapp_config($option) {
     global $_CONF;
-
+## TODO add debug_log
     if (! isset($_CONF) || is_null($_CONF) )
         if (file_exists('config.ini') )
             $_CONF = parse_ini_file('config.ini');
         else
             die('Config file not found');
 
-    if (isset($_CONF[$option]))
+    if (isset($_CONF[$option])) {
         return $_CONF[$option];
-    else
+    } else
         die("Config option $option not found");
 }
 
 /**
  * Returns a value of particular variable from global GET or POST array.
  *
- * @param string @string variable name
+ * @param string $string variable name
  *
  * @param string $method http method [POST|post|GET|get] default = ''
  *
  * @return mixed variable value
  */
 function onapp_get_arg($string, $method='') {
+
+    onapp_debug("Get arguments" );
+
     switch($method) {
         case "get":
         case "GET":
-            return array_key_exists($string, $_GET) ? $_GET[$string] : NULL;
+            $return = array_key_exists($string, $_GET) ? $_GET[$string] : NULL;
             break;
         case "post":
         case "POST":
-            return array_key_exists($string, $_POST) ? $_POST[$string] : NULL;
+            $return = array_key_exists($string, $_POST) ? $_POST[$string] : NULL;
             break;
        case "session":
        case "SESSION":
-            return $_SESSION;
+            $return = $_SESSION;
             break;
         default :
-            $value_post = onapp_get_arg($string,"POST");
-            $value_get = onapp_get_arg($string,"GET");
-            return $value_post ? $value_post : $value_get;
+            $value_post = array_key_exists($string, $_POST) ? $_POST[$string] : NULL;
+            $value_get  = array_key_exists($string, $_GET) ? $_GET[$string] : NULL;
+            $return = $value_post ? $value_post : $value_get;
             break;
     }
+
+    onapp_debug("onapp_get_arg: string => $string, method => $method, return => $return" );
+
+    return $return;
 }
 
 /**
@@ -97,13 +119,16 @@ function onapp_get_arg($string, $method='') {
  * @param array $params Array consists of variables' keys and values need to be
  * passed to Smarty template view file.
  *
- * @todo Realise muliple template system.
+ * @todo  VERIFICATION IF TEMPLATE FILE EXSITS!
  *
  * @return void
  *
  */
 function onapp_show_template($view, $params = array()) {
     global $_ALIASES, $_SCREEN_IDS;
+
+    onapp_debug("Show templates");
+    onapp_debug("onapp_show_template: view => $view, params => " . print_r($params, true));
 
     $template = onapp_config('TEMPLATE').DIRECTORY_SEPARATOR.str_replace('_',DIRECTORY_SEPARATOR,$view).'.tpl';
 
@@ -127,6 +152,8 @@ function onapp_show_template($view, $params = array()) {
         $smarty->assign("$key",$value);
 
     $smarty->display($template);
+
+    onapp_debug("Display Smarty Template");
 }
 
 /**
@@ -140,6 +167,8 @@ function onapp_show_template($view, $params = array()) {
  */
 function onapp_load_language($lang = '') {
     global $_LANG;
+
+    onapp_debug("Load language: lang => '$lang'");
 
     if(isset($_SESSION["language"]) && $_SESSION["language"] != '')
         $language = $_SESSION["language"];
@@ -173,6 +202,18 @@ function onapp_load_screen_ids($SimpleXMLElement = null, $parrent_id = '') {
         foreach($SimpleXMLElement->screen[ $id -1 ]->attributes() as $k=>$v)
             $_SCREEN_IDS["$current_id"][$k] = (String)$v;
 
+        if(isset($_SCREEN_IDS["$current_id"]['title'])){
+            $file_path = 'controllers/c_'.strtolower($_SCREEN_IDS["$current_id"]['class']).'.php';
+            if(file_exists($file_path))
+            {
+                require_once $file_path;
+                    //todo verify if exists class and function
+                $_SCREEN_IDS["$current_id"]['show'] = call_user_func(array($_SCREEN_IDS["$current_id"]['class'], $_SCREEN_IDS["$current_id"]['access']));
+            }
+            else
+                die('File '.$file_path.' doesn\'t exists');
+
+        }
         $_ALIASES[$_SCREEN_IDS["$current_id"]["alias"]] = $current_id;
 
         onapp_load_screen_ids($SimpleXMLElement->screen[ $id -1 ], "$current_id");
@@ -219,6 +260,8 @@ function onapp_cryptData($value, $action) {
  *
  */
 function onapp_redirect($url) {
+    onapp_debug("Redirect: url => '$url'");
+
     if (!headers_sent()){    //If headers not sent yet... then do php redirect
         header('Location: '.$url); exit;
     }else{                    //If headers are sent... do java redirect... if java disabled, do html redirect.
@@ -240,8 +283,9 @@ function onapp_redirect($url) {
  */
 function onapp_check_configs() {
     require_once "wrapper/Profile.php";
+    onapp_startSession();
 
-    session_start();
+    onapp_debug("Check configs");
 
     if(!$_SESSION)
     {
@@ -291,16 +335,17 @@ function onapp_check_configs() {
  * @return void
  *
  */
-function onapp_startSession($time = '3600', $ses = 'MYSES') {
-/*
+function onapp_startSession($ses = 'MYSES') { 
+    $time =  onapp_config('SESSION_LIFETIME');
     session_set_cookie_params($time);
     session_name($ses);
     session_start();
 
+    onapp_debug("Start SESSION");
+
     // Reset the expiration time upon page load
     if (isset($_COOKIE[$ses]))
       setcookie($ses, $_COOKIE[$ses], time() + $time, "/");
-*/
 }
 
 /**
@@ -317,30 +362,243 @@ function onapp_is_auth() {
         && isset($_SESSION["host"])
         && isset($_SESSION["id"]);
 
+    onapp_debug("onapp_is_auth: return => '$is_auth'");
+
     return $is_auth;
 }
+
 /**
+ * Writes string into file
  *
- * Not in use yet
+ * @param string $filename File name
+ * @param string $content  Content string
  *
  * @return void
  *
- * @todo realize function
+ */
+function onapp_file_write($type, $content) {
+    $log_directory = 'logs';
+
+    switch ($type) {
+        case 'frontend':
+            $filename = "$log_directory/fronted.log";
+            break;
+        case 'wrapper':
+        case 'onapp':
+            $filename = "$log_directory/wrapper.log";
+            break;
+        case 'third':
+            $filename = "$log_directory/third.log";
+            break;
+        case 'error':
+            $filename = "$log_directory/errors/error_".$_SESSION['log_id'].'.log';
+            break;
+        default:
+            return;
+            break;
+    }
+
+    if (!$handle = fopen($filename, 'a+'))
+    {
+         echo "Cannot open file ($filename)in file ".__FILE__.' line '.__LINE__;
+         exit;
+    }
+
+    if (fwrite($handle, $content."\n") === FALSE)
+    {
+        echo "Cannot write to file ($filename)";
+        exit;
+    }
+
+    fclose($handle);
+}
+
+/**
+ *
+ * Inits log id and logs levels
+ *
+ * @global array $_LOG_LEVELS
+ *
+ * @return void
  *
  */
-function onapp_access() {
-/*
-     // Load profile object
-      $profile = $this->onapp->factory('Profile');
-      $profile_obj = $profile->load();
+function onapp_init_log()
+{
+    global $_LOG_LEVELS;
 
-      $access = false;
+    $_SESSION['log_id'] = substr(md5($_SESSION['id'] . date('d-m-Y H-i-s') ), -10);
 
-      foreach( $profile_obj->_roles as $value )
-                    if($value->_permissions->identifier == str_replace( '_', '.', self::called_function ))// $this->called_function = __FUNCITON__;
-                            $access = true;
-                    else
-                        ;
-      return $access;
-*/
+    $_LOG_LEVELS = array(
+        'FATAL' => 1,
+        'ERROR' => 2,
+        'WARN'  => 3,
+        'INFO'  => 4,
+        'DEBUG' => 5
+    );
+
+    onapp_debug("Initialise frontend loger");
+}
+
+/**
+ * Adds onapp debug.
+ *
+ * @param string $message Debug message
+ *
+ * @param mixed $obj Mixed variable to print
+ *
+ * @return void
+ *
+ */
+function onapp_debug( $message )
+{
+    global $_LOG_LEVELS;
+
+    if( onapp_config('LOG_LEVEL') < $_LOG_LEVELS['DEBUG'] || ! isset( $_SESSION['log_id'] ) )
+        return;
+
+    $msg = $_SESSION['log_id']." : [DEBUG] $message";
+
+    onapp_file_write('frontend', $msg);
+}
+
+/**
+ * Adds onapp info.
+ *
+ * @param string $message Information message
+ *
+ * @param mixed $obj Mixed variable to print
+ *
+ * @return void
+ *
+ */
+function onapp_info( $message )
+{
+    global $_LOG_LEVELS;
+
+    if( onapp_config('LOG_LEVEL') < $_LOG_LEVELS['INFO'] )
+        return;
+
+    $msg = $_SESSION['log_id']." : [INFO] $message";
+
+    onapp_file_write('frontend', $msg);
+}
+
+/**
+ * Adds onapp warn message.
+ *
+ * @param string $message Warning message
+ *
+ * @return void
+ *
+ */
+function onapp_warn( $message )
+{
+    global $_LOG_LEVELS;
+
+    if( onapp_config('LOG_LEVEL') < $_LOG_LEVELS['WARN'])
+        return;
+
+    $msg = $_SESSION['log_id']." : [WARN] $message";
+
+    onapp_file_write('error', $msg);
+
+    die('WARNING');
+
+//    onapp_redirect(BASEURL.'/errors/error500.php?log_id='.$_SESSION['log_id']);
+}
+
+/**
+ * Adds onapp error message.
+ *
+ * @param string $message Error message
+ *
+ * @return void
+ *
+ */
+function onapp_error( $message )
+{
+    global $_ALIASES, $_LOG_LEVELS;
+
+    if( onapp_config('LOG_LEVEL') < $_LOG_LEVELS['ERROR'] )
+        return;
+
+    $msg = $_SESSION['log_id']." : [ERROR] $message";
+
+    onapp_file_write('error', $msg);
+
+    die('ERROR');
+
+//    onapp_redirect(BASEURL.'/'
+//                   .($_ALIASES['profile'])
+//                   .'?error=ERROR ID - '
+//                   .$_SESSION['log_id']
+//                   .'&no_error_translate=true');
+}
+
+/**
+ * Adds onapp fatal message.
+ *
+ * @param string $message Fatal error message
+ *
+ * @return void
+ *
+ */
+function onapp_fatal($message)
+{
+    global $_LOG_LEVELS;
+
+    if( onapp_config('LOG_LEVEL') < $_LOG_LEVELS['FATAL'] )
+            return;
+
+    $msg = $_SESSION['log_id']." : [FATAL] $message";
+
+    onapp_file_write('error', $msg);
+
+    die('FATAL');
+
+//    onapp_redirect(BASEURL.'/errors/error500.php?log_id='.$_SESSION['log_id']);
+}
+
+/**
+ *
+ * Checks whether user has permission to perform particular action
+ *
+ * @param string $permission
+ *
+ * @return void
+ *
+ */
+function onapp_permission($permissions) {
+    global $_ALIASES;
+    if(is_array($permissions))
+    {
+        foreach($permissions as $permission)
+            if(in_array($permission, $_SESSION['permissions']))
+                return;
+    }
+    else
+        in_array($permissions, $_SESSION['permissions']);
+
+    $error = 'YOU_HAVE_NOT_PERMISSION_FOR_THIS_ACTION';
+    onapp_redirect(BASEURL.'/'.($_ALIASES['profile']).'?error='.$error);
+}
+
+/**
+ * Checks whether user has permission to view particular items
+ *
+ * @param string $permission
+ *
+ * @return boolean [true|false]
+ *
+ */
+function onapp_has_permission($permissions) {
+    if(is_array($permissions))
+    {
+           foreach($permissions as $permission)
+               if(in_array($permission, $_SESSION['permissions']))
+                       return true;
+
+               return false;
+    }
+    return in_array($permissions, $_SESSION['permissions']);
 }
