404132892736a3b18dbeae634b8e7dc2fc0e2d38
Andrew Yatskovets <jtsandrew@gmail.com>
2011-06-03 18:06:49 +0300

    Added strings to lang file

--- lang/English/strings.php
+++ lang/English/strings.php
@@ -1,5 +1,6 @@
 <?php
-$_LANG['ERORR_WRONG_LOGIN_DATA'] = 'Login, password or host incorrect';
+$_LANG['CHARSET']='utf-8';
+$_LANG['ERORR_WRONG_LOGIN_DATA'] = 'Login, password or host is incorrect';
 $_LANG['HELLO_'] = 'Hello';
 $_LANG['VITRUAL_MACHINE_DETAILS'] = 'Virtual Machine Details';
 $_LANG['CREATE_VIRTUAL_MACHINE'] = 'Create Virtual Machine';
@@ -9,7 +10,6 @@ $_LANG['HOST_'] = 'Host';
 $_LANG['SELECT_LANG'] = 'Select language';
 $_LANG['LOGOUT_'] = 'Log Out';
 $_LANG['DASHBOARD_'] = 'Dashboard';
-
 $_LANG['USER_DETAILS'] = 'User Details';
 $_LANG['FULL_NAME'] = 'Fullname';
 $_LANG['E_MAIL'] = 'E-mail';
@@ -19,4 +19,111 @@ $_LANG['USER_ROLES'] = 'User Roles';
 $_LANG['YOUR_PROFILE_INFORMATION'] = 'Your profile information';
 $_LANG['YOUR_PROFILE'] = 'Your profile';
 $_LANG['PROFILE_'] = 'Profile';
-?>
+$_LANG['VIRTUAL_MACHINES'] = 'Virtual Machines';
+$_LANG['LOAD_BALANCERS'] = 'Load Balancers';
+$_LANG['HYPERVISORS_'] = 'Hypervisors';
+$_LANG['TEMPLATES_'] = 'Templates';
+$_LANG['TEMPLATE_LIST'] = 'Template List';
+$_LANG['SOFTWARE_LICENSES'] = 'Software Licenses';
+$_LANG['TEMPLATE_STORE'] = 'Template Store';
+$_LANG['LOGS_'] = 'Logs';
+$_LANG['SETTINGS_'] = 'Settings';
+$_LANG['USERS_AND_GROUPS'] = 'Users and Groups';
+$_LANG['VIRTUAL_MACHINE_HAS_BEEN_CREATED_SUCCESSFULLY'] = 'Virtual machine has been created successfully';
+$_LANG['TOOLS_'] = 'Tools';
+$_LANG['ALERTS_'] = 'Alerts';
+$_LANG['USAGE_STATISTICS'] = 'Usage Statistics';
+$_LANG['SYSADMIN_TOOLS'] = 'Sysadmin Tools';
+$_LANG['HELP_'] = 'Help';
+$_LANG['ALERTS'] = 'Alerts';
+$_LANG['ADD_NEW_VIRTUAL_MACHINE'] = 'Add New Virtual Machine';
+$_LANG['VIRTUAL_MACHINES'] = 'Virtual Machines';
+$_LANG['VIRTUAL_MACHINE_WILL_BE_STOPED_SHORTLY'] = 'Virtual machine will be stoped shortly';
+$_LANG['VIRTUAL_MACHINE_WILL_BE_REBOOTED_SHORTLY'] = 'Virtual machine will be rebooted shortly';
+$_LANG['VIRTUAL_MACHINE_STARTUP_HAS_BEEN_QUEUED'] = 'Virtual machine startup has been queued';
+$_LANG['YOU_HAVE_NOT_PERMISSION_FOR_THIS_ACTION'] = 'You havn\'t permission for this action';
+$_LANG['VIRTUAL_MACHINE_PASSWORD_WIL_BE_CHANGED_SHORTLY'] = 'Virtual machine password will be changed shortly';
+$_LANG['VIRTUAL_MACHINE_WILL_BE_SHUTEDED_DOWN_SHORTLY'] = 'Virtual machine will be shuted down shortly';
+$_LANG['ARE_YOU_SURE_YOU_WONT_RESET_ROOT_PASSWORD'] = 'Are you sure you want reset root password ?';
+$_LANG['LABEL_'] = 'Label';
+$_LANG['INSTRUCTION_RESOURCE_ALLOCATIONS'] = 'Change this virtual machine\'s resource allocations as required, then click the Save Virtual Machine button.';
+$_LANG['ADJUST_RESOURCE_ALLOCATIONS'] = 'Adjust Resource Allocations';
+$_LANG['VIRTUAL_MACHINES_PROPERTIES'] = 'Virtual Machines Properties';
+$_LANG['HYPERVISOR_ZONE'] = 'Hypervisor Zone';
+$_LANG['HOST_NAME'] = 'Hostname';
+$_LANG['HYPERVISOR_'] = 'Hypervisor';
+$_LANG['OPERATING_SYSTEM'] = 'Operating System';
+$_LANG['DISTRIBUTION'] = 'Distribution';
+$_LANG['TEMPLATE_'] = 'Template';
+$_LANG['RESOURCES'] = 'Resources';
+$_LANG['RAM_'] = 'RAM';
+$_LANG['CPU_CORES'] = 'Cpu Cores';
+$_LANG['CPU_PRIORITY'] = 'Cpu Priority';
+$_LANG['CPU_USAGE'] = 'Cpu Usage';
+$_LANG['DATA_STORE_ZONE'] = 'Data Store Zone';
+$_LANG['SWAP_DISK_SIZE'] = 'Swap Disk Size';
+$_LANG['PRIMARY_DISK'] = 'Primary Disk';
+$_LANG['NETWORK_CONFIGURATION'] = 'Network Configuration';
+$_LANG['NETWORK_ZONE'] = 'Network Zone';
+$_LANG['PORT_SPEED'] = 'Port Speed';
+$_LANG['AUTOMATION_SETTINGS'] = 'Automation Settings';
+$_LANG['REQUIRED_AUTOMATIC_BACKUP'] = 'Required Automatic Backup';
+$_LANG['BUILD_VIRTUAL_MACHINE_AUTOMATICALLY'] = 'Build Virtual Machine Automatically';
+$_LANG['PRICE'] = 'Price';
+$_LANG['PRICE_PER_HOUR'] = 'Price Per Hour';
+$_LANG['INSTRUCTION_VIRTUAL_MACHINE_NEW'] = 'Your virtual machine will be sent for processing immediately. You will be redirected to your VM\'s information screen with details of the build process.';
+$_LANG['SWAP_DISK'] = 'Swap Disk';
+$_LANG['PRIMARY_DISK_SIZE'] = 'Primary Disk Size';
+$_LANG['BACKUPS_'] = 'Backups';
+$_LANG['VIRTUAL_MACHINES_DEFAULT_INFO'] = ' The virtual machines in your cloud.
+                                            Click a VM\'s Label to see its details.
+                                            Use the On/Off buttons to change Power status.
+                                            Use the icons on the right to manage backups and see VM resource utilization.
+                                            Add new VMs with the Add Virtual Machine button.';
+$_LANG['VIRTUAL_MACHINES_DETAILS_INFO'] = ' The On/Off buttons change this virtual machine\'s power status. 
+                                            The Actions tools let you perform common tasks. 
+                                            The activity log shows VM transactions and lets you cancel pending tasks. 
+                                            Use the navigation at the top of the screen to explore different aspects of this virtual machine.';
+$_LANG['VIRTUAL_MACHINES_NEW_INFO_2'] = ' Your virtual machine will be sent for processing immediately.
+                                          You will be redirected to your VM\'s information screen with details of the build process.';
+$_LANG['CONFIGURATION_'] = 'Configuration';
+$_LANG['SCREENS_'] = 'Screens';
+$_LANG['LOGS_SETTINGS'] = 'Logs Settings';
+$_LANG['SCREEN_ID'] = 'Screen Id';
+$_LANG['VIEW_'] = 'View';
+$_LANG['THERE_IS_NO_SUCH_SCREEN'] = 'There is no such screen';
+
+$_LANG['SCREEN_DESCRIPTION'] = 'Screen Description';
+$_LANG['SCREEN_CLASS'] = 'Screen Class';
+$_LANG['SCREEN_METHOD'] = 'Screen Method';
+$_LANG['SCREEN_INFO'] = 'Screen Info';
+
+$_LANG['PARENT_SCREEN_ALIAS'] = 'Parent Screen Alias';
+$_LANG['SCREENS_INFO_INFO'] = 'Insert screen id and click the view button to see details.';
+$_LANG['LOGS_SETTINGS'] = 'Logs Settings';
+$_LANG['LOGS_SETTINGS_INFO'] = 'Make necessary changes and click save button';
+$_LANG['LOG_LEVEL'] = 'Log Level';
+$_LANG['SAVE_'] = 'Save';
+$_LANG['ENABLE_THIRD_PART_PRODUCT_REPORT'] = 'Enable third part product report';
+$_LANG['ENABLE_DEBUG_LOG_IN_PROBLEM_REPORT'] = 'Enable debug log in problem report';
+$_LANG['ENABLE_WRAPPER_LOG_REPORT'] = 'Enable wrapper log report';
+$_LANG['COULD_NOT_UPDATE_CONFIG_FILE'] = 'Couldn\'t update config file';
+$_LANG['CONFIGURATIONS_HAVE_BEEN_UPDATED'] = 'Configurations have been updated';
+$_LANG['HYPERVISORS_INFO'] = 'Hypervisors provide CPU, 
+                              RAM and network resources for your virtual machines.
+                              Click a hypervisor label to show the virtual machines running on it.';
+$_LANG['HYPERVISOR_DETAILS_INFO'] = 'The virtual machines on %s hypervisor.
+                              The On/Off buttons change virtual machine Power status. 
+                              The icons on the right let you manage
+                              VM Backup status and see resource utilization charts.';
+$_LANG['HYPERVISOR_DETAILS'] = 'Hypervisors Details';
+$_LANG['CPU_USAGE_FOR_THIS_VM'] = 'CPU usage for this virtual machine';
+$_LANG['CPU_USAGE_FOR_THIS_VM_INFO'] = 'This chart shows hourly 
+                                         utilization for the period up to three months. You can zoom into a 
+                                         specific time period by clicking and dragging in a chart.';
+
+
+
+                              
+
+
