d22a047d26798fafe56608af6f197c140e6870e9
Andrew Yatskovets <jtsandrew@gmail.com>
2011-06-03 18:17:18 +0300

    Changed index.php

--- index.php
+++ index.php
@@ -4,23 +4,15 @@ require_once 'functions.php';
 error_reporting( E_ALL );
 ini_set( 'display_errors', 0 );
 
-
-
-register_shutdown_function( 'onapp_error' );
-set_error_handler( 'onapp_fatal' );
+set_error_handler( 'onapp_error_handler' );
+register_shutdown_function( 'onapp_fatal' );
 
 require_once 'ONAPP_Controller.php';
 
-init_config();
+onapp_init_config();
 onapp_check_configs();
-
 onapp_init_log();
-
-onapp_debug('Load Screen IDs');
-onapp_load_screen_ids();
-
-onapp_debug(print_r($_ALIASES, true));
-onapp_debug(print_r($_SCREEN_IDS, true));
+onapp_init_screen_ids();
 
 onapp_load_language(ONAPP_DEFAULT_LANGUAGE);   
 
