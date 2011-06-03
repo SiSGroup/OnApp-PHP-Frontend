1d899b898b78a24b77c4996032faa1e665e91dfd
Andrew Yatskovets <jtsandrew@gmail.com>
2011-06-03 18:33:49 +0300

    Changed class ONAPP_Controller

--- ONAPP_Controller.php
+++ ONAPP_Controller.php
@@ -11,11 +11,13 @@ Class ONAPP_Controller
 
         $route = onapp_get_arg('route');
 
+        onapp_debug("ONAPP_Controller->__construct: route => $route");
+
         if( onapp_is_auth() && $_SCREEN_IDS[$route]['alias'] == 'login')
-            onapp_redirect( BASEURL.'/'.$_ALIASES[onapp_config('DEFAULT_ALIAS')] );
+            onapp_redirect( ONAPP_BASE_URL.'/'.$_ALIASES[ONAPP_DEFAULT_ALIAS] );
         elseif ( ! onapp_is_auth() && $_SCREEN_IDS[$route]['alias'] != 'login' )
-            // TODO save url before redirection for future redirection back after sucess logine
-            onapp_redirect( BASEURL.'/'.$_ALIASES['login'] );
+            // TODO save url before redirection for future redirection back after sucess login
+            onapp_redirect( ONAPP_BASE_URL.'/'.$_ALIASES['login'] );
     }
 
     public function access() {
@@ -33,14 +35,25 @@ Class ONAPP_Controller
         else if(is_null($route))
             onapp_redirect( $_ALIASES["profile"]);
         else
-            onapp_redirect(BASEURL.'/error404.php');
+            onapp_redirect(ONAPP_BASE_URL.'/errors/error404.php');
+
+        if ( ! defined('ONAPP_BASE_URL'))
+            onapp_error('No direct script access allowed');
 
         $file_path = 'controllers/'.strtolower('c_'.$class_name.'').'.php';
 
         if(file_exists($file_path))
             require_once "$file_path";
         else
-            die("Could not find file $file_path");
+            onapp_error("Could not find file $file_path");
+
+        onapp_debug("ONAPP_Controller->run: args => " . print_r(
+            array(
+                'route'  => $route,
+                'file'   => $file_path,
+                'class'  => $class_name,
+                'method' => $method_name
+            ), true) );
 
         $new_class = new $class_name();
         $new_class->$method_name();
