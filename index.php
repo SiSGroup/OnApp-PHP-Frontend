b89f46e268600cb72157442784cd57de640c1133
Andrew Yatskovets <jtsandrew@gmail.com>
2011-06-08 14:47:58 +0300

    Changed error login procedure

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
 require_once 'functions.php';
-
-error_reporting( E_ALL );
-ini_set( 'display_errors', 0 );
-
-set_error_handler( 'onapp_error_handler' );
-register_shutdown_function( 'onapp_fatal' );
+require_once 'error_log.php';
 
 require_once 'ONAPP_Controller.php';
 
 onapp_init_config();
 onapp_check_configs();
+
 onapp_init_log();
+
 onapp_init_screen_ids();
 
-onapp_load_language(ONAPP_DEFAULT_LANGUAGE);   
+onapp_load_language(ONAPP_DEFAULT_LANGUAGE);
 
 $controller = new ONAPP_Controller();
 $controller->run();
