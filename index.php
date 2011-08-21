1a312b7ae998c811d6966286ee9eaeae4b33d9ef
Yurik <yurik@ubuntu.ubuntu-domain>
2011-08-21 23:41:26 +0300

    Fix

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
+
+define( 'ONAPP_PATH', dirname(__FILE__) );
 define( 'ONAPP_DS', DIRECTORY_SEPARATOR );
-define( 'ONAPP_PATH', dirname( __FILE__ ) );
 
-
-require_once ONAPP_PATH . ONAPP_DS . 'install.php'; 
-require_once ONAPP_PATH . ONAPP_DS . 'wrapper' . ONAPP_DS . 'OnAppInit.php'; 
+require_once ONAPP_PATH . ONAPP_DS . 'install.php';
+require_once ONAPP_PATH . ONAPP_DS . 'wrapper' . ONAPP_DS . 'OnAppInit.php';
 require_once ONAPP_PATH . ONAPP_DS . 'functions.php';
-onapp_init_config( ); 
 require_once ONAPP_PATH . ONAPP_DS . 'error_log.php';
 require_once ONAPP_PATH . ONAPP_DS . 'ONAPP_Controller.php';
 
-onapp_startSession( ); 
-onapp_check_configs( );         
-onapp_init_log( );
-onapp_init_screen_ids( );
-onapp_load_language( ONAPP_DEFAULT_LANGUAGE );   
+onapp_init_config();
+onapp_startSession();
+onapp_check_configs();
+onapp_init_log();
+onapp_init_screen_ids();
+onapp_load_language(ONAPP_DEFAULT_LANGUAGE);
 
-$controller = new ONAPP_Controller( );
-$controller->run( );
+$controller = new ONAPP_Controller();
+$controller->run();
