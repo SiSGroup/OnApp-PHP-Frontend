f03bcfafdfdebf1827a9eabec43431e51a4c1012
Yuriy Yakubskiy <yuriy.yakubskiy@onapp.com>
2011-08-23 10:47:20 +0300

    Little changes

--- 1.php
+++ 1.php
@@ -4,23 +4,39 @@
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf8" />
     <title>Installation</title>
+  <style type="text/css">
+        table, .table {
+            width:900px;
+            background: #f6f6f6;
+            padding:10px;
+            margin:0 auto;
+        }
 
+        .yes{
+            color:green;
+            font-weight:bold
+        }
+        .red{
+            color:red;
+
+        }
+    </style>
 </head>
 <body>
-This page reminds you to set correctly you virtual host in order your .htaccess file work in this directory!<br />
+    <div class="table">This page reminds you to set correctly you virtual host in order your .htaccess file work in this directory!<br />
 
 You must change <b>'AllowOverride None'</b> to <b>'AllowOverride All'</b> in you server config file!
 <br />
 
-<?php echo '<pre>
+ <pre>
 DocumentRoot /var/www /:yourhost
-        <Directory /var/www/:yourhost>
+    <?php   echo htmlentities( '<Directory /var/www/:yourhost>
                 Options Indexes FollowSymLinks MultiViews
                 AllowOverride <b style="color:red">All</b>
                 Order allow,deny
                 allow from all
-        </Directory>
-</pre>'; ?>
+        </Directory>' ); ?>
+</pre>
 
 <?php $base = (
             ( isset($_SERVER['HTTPS'])
@@ -33,5 +49,6 @@ DocumentRoot /var/www /:yourhost
 $base = preg_replace('/1|index.php/', '', $base);?>
 
 <b>If everything good restart your server, delete or rename "1.php" file</b> and follow the link  <a href="<?php echo $base ?>" >Link</a>
+    </div>
 </body>
 </html>
